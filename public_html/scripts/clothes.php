617f1f56d764bde7ba459788b2f8fa01509b3416
Greenbro-net <greenbro.net@gmail.com>
2020-03-03 23:58:11 +0200

    New commit

--- public_html/scripts/clothes.php
+++ public_html/scripts/clothes.php
@@ -8,7 +8,7 @@ require_once('link.php');
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     
     <meta charset="UTF-8">
-    <title>Greenbro - одяг</title>
+    <title>Greenbro - одяг1</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="//fonts.googleapis.com/css?family=Roboto:400,700&amp;subset=cyrillic" rel="stylesheet">
