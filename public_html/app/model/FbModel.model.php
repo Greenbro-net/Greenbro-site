28d0365e3de6c0f69979d6bdceade97128fb3c90
Greenbro-net <greenbro.net@gmail.com>
2021-02-11 11:20:34 +0200

    fb_conf

--- public_html/app/model/FbModel.model.php
+++ public_html/app/model/FbModel.model.php
@@ -1,10 +1,17 @@
 <?php
 
-class FBLoginModel
+class FbModel 
 {
-    public function do() 
+    public function grab_fb_object() 
     {
         try {
+             $result_grab_fb = require_once '../fb_config.php';
+             if (empty($result_grab_fb)) {
+                 throw new Exception("FB config file wasn't require");
+             } 
+             elseif (!empty($facebook)) {
+                 return $facebook;
+                }
    
         } catch (Exception $exception) {
             file_put_contents("my-errors.log", 'Message:' . $exception->getMessage() . '<br />'.   'File: ' . $exception->getFile() . '<br />' .
