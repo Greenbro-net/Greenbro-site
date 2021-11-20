661d7c0ed5d57980bf9335b465d1f1b61ca91fd9
“alexander” <greenbro.net@gmail.com>
2021-11-21 00:01:55 +0200

    Refactor ItemModel

---
+++ fb_config.php
@@ -0,0 +1,8 @@
+<?php
+
+$facebook = new \Facebook\Facebook([
+    'app_id' => '884265225709842',
+    'app_secret' => '5488ac1cd86905f62315e7de312b5678',
+    'default_graph_version' => 'v9.0'
+    ]);
+    
