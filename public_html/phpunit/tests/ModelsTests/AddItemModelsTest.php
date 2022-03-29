040d01766e3ecba14fd9d11b39d40c5d712beefe
“alexander” <greenbro.net@gmail.com>
2022-03-29 23:59:39 +0300

    Add tests for AddItemModelsTest

---
+++ public_html/phpunit/tests/ModelsTests/AddItemModelsTest.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\UnitTest;
+
+
+use PHPUnit\Framework\TestCase;
+
+
+class AddItemModelsTest extends TestCase
+{   
+    // the test method below for get_last_products_id() 
+    /** @test */
+    public function mock_get_last_products_id()
+    {
+        $AddItemModelObject = new \App\Model\AddItemModel();
+        $ResultOfMethod = $AddItemModelObject->get_last_products_id();
+        $this->assertIsInt($ResultOfMethod);
+    }
+
+    // the test method below for check_existing_of_file()
+    /** @test */
+    public function mock_check_existing_of_file()
+    {
+        $AddItemModelObject = new \App\Model\AddItemModel();
+        $ResultOfMethod = $AddItemModelObject->check_existing_of_file("../images/AAA.jpg");
+        $this->assertEmpty($ResultOfMethod);
+    }
+    
+    // the test method below for is_file_image()
+    /** @test */
+    public function mock_check_file_type()
+    {
+        $AddItemModelObject = new \App\Model\AddItemModel();
+        $ResultOfMethod = $AddItemModelObject->check_file_type("../images/AAA.jpg");
+        $this->assertEmpty($ResultOfMethod);
+    }
+    
+}
